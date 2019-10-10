7220cf8d0980c50dc12e3f974c589fc69f71a14d
David Queen <dsqjr@msn.com>
2019-10-10 18:39:19 -0400

    submitted for grading

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,7 +3,16 @@ import '../css/grid.css';
 import me from '../img/me.jpg';
 import Menu from '../scripts/menu';
 import { TweenMax } from 'gsap';
-import siema from 'siema';
+
+//hambuger button
+let mainNav =document.getElementsByTagName('nav')[0];
+let hamb = document.getElementById('hamb');
+hamb.addEventListener('click',(e)=>{
+  mainNav.style.display = 'grid';
+});
+mainNav.addEventListener('focusout',(e)=>{
+  mainNav.style.display = 'none';
+});
 
 document.getElementById('me').src = me;
 let buttons = [];
@@ -12,7 +21,7 @@ let buttons = [];
 let menu = new Menu();
 buttons = menu.createButtons('about_me','my_skills','my_life','my_projects','contact_me');
 menu.animate();
-//document.getElementById('hamb').addEventListener('click',()=>);
+//animations
 window.menu = menu;//testing purposes
 let hello = document.getElementById('welcome');
 TweenMax.from(hello,5,{opacity:0});
